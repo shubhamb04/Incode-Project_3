f32b5ae2fd6c93ed5b273c4ccdd2f4f4fa94c97a
Shubham Bhavsar <shubhambhavsar50@gmail.com>
2021-08-22 23:09:41 +1000

    learning express.js

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
+//bringing in express
 const express = require("express");
-const app = express()
-const data = require("./data")
-const PORT = 3000
-console.log(data)
 
+//bringing in path module
+const path = require("path")
+
+//Initialise the app
+const app = express()
+
+const data = require("./data")
+
+//listen on a port
+const PORT = process.env.PORT || 3000;
+
+// console.log(data)
+
+//create your endpoints/route handlers
 app.get("/", (req, res) => {
-    res.send("Hello World!")
+    //sending a response
+    res.send("<h1>Hello World......!</h1>")
+
+    //sending a file in response
+    // res.sendFile(path.join(__DirName, 'public', 'index.html'))
 })
+
 
 app.listen(PORT, () => {
     console.log(`Here is the app at http://localhost:${PORT}`);
