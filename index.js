88f83261d67f97078ed9772322ed5fc38b98ae40
Shubham Bhavsar <shubhambhavsar50@gmail.com>
2021-08-24 20:37:00 +1000

    created routes to get data

--- index.js
+++ index.js
@@ -1,34 +1,82 @@
-//bringing in express
 const express = require("express");
-
-//bringing in path module
-const path = require("path")
+const path = require("path");
+const logger = require("./middleware/logger");
 
 //Initialise the app
-const app = express()
+const app = express();
 
-const data = require("./data")
+const data = require("./data");
 
 //listen on a port
 const PORT = process.env.PORT || 3000;
 
 // console.log(data)
 
+//Init middleware
+// app.use(logger);
+
 //create your endpoints/route handlers
 app.get("/", (req, res) => {
-    //sending a response
-    res.send('Welcome to our schedule website')
-
-    //sending a file in response
-    // res.sendFile(path.join(__DirName, 'public', 'index.html'))
-})
+  res.send("Welcome to our schedule website");
+  //sending a file in response
+  //   res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 //gets all users
-app.get('/users', (req, res) => {
-    res.json(data.users)
-})
+app.route("/users").get((req, res) => {
+  res.send(data.users);
+});
 
+//get individual users
+app.get("/users/:id", (req, res) => {
+  const found = data.users.some(
+    (user) => data.users.indexOf(user) === parseInt(req.params.id)
+  );
+
+  if (found) {
+    res.json(
+      data.users.filter(
+        (user) => data.users.indexOf(user) === parseInt(req.params.id)
+      )
+    );
+  } else {
+    res
+      .status(400)
+      .json({ msg: `No user found with the id of ${req.params.id}` });
+  }
+});
+
+//get schedules of individual users
+app.get("/users/:id/schedules", (req, res) => {
+  const foundUser = data.users.some(
+    (user) => data.users.indexOf(user) === parseInt(req.params.id)
+  );
+  const foundSchedule = data.schedules.some(
+    (sch) => sch.user_id === parseInt(req.params.id)
+  );
+
+  if (foundUser) {
+    if (foundSchedule) {
+      res.json(
+        data.schedules.filter((sch) => sch.user_id === parseInt(req.params.id))
+      );
+    } else {
+      res
+        .status(400)
+        .json({ msg: `No schedule exist with the id of ${req.params.id}` });
+    }
+  } else {
+    res
+      .status(400)
+      .json({ msg: `No user exist with the id of ${req.params.id}` });
+  }
+});
+
+//get all schedules
+app.route("/schedules").get((req, res) => {
+  res.send(data.schedules);
+});
 
 app.listen(PORT, () => {
-    console.log(`Here is the app at http://localhost:${PORT}`);
-})
+  console.log(`Here is the app at http://localhost:${PORT}`);
+});
