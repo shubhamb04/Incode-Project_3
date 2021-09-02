b553d2d9d9e07d2f1fc104c5529aa6bf3b05d7d7
Shubham Bhavsar <shubhambhavsar50@gmail.com>
2021-09-02 22:11:16 +1000

    Implemented input validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const db = require("../../db/database");
-
+const {
+  check,
+  expressValidator,
+  validationResult,
+} = require("express-validator");
 
 //get all schedules
 router.get("/schedules", async (req, res) => {
@@ -39,23 +43,36 @@ router.get("/new-schedule", (req, res) => {
 });
 
 //Add new schedule
-router.post("/schedules", async (req, res) => {
-  try {
-    const { username, day, start_at, end_at } = req.body;
-  if (!username || !day || !start_at || !end_at) {
-    return res.render("pages/error", { msg: "Please fill all the details!" });
-  }
+router.post(
+  "/schedules",
+  [
+    check("username", "Please enter valid username!")
+      .exists()
+      .isLength({ min: 3 }),
+    check("day", "Please select the day!").notEmpty(),
+    check("start_at", "Please enter valid start time!").notEmpty(),
+    check("end_at", "Please enter valid end time!").notEmpty(),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
 
-    const result = await db.query("INSERT INTO schedules (username, day, start_at, end_at) VALUES($1, $2, $3, $4)",
-    [username, day, start_at, end_at]);
-        
-    res.redirect("/schedules");
-    
-  
-  } catch (error) {
-    console.log(error);
+      const { username, day, start_at, end_at } = req.body;
+      if (!errors.isEmpty()) {
+        const errMsgs = errors.array();
+        res.render("pages/add_schedule", { errMsgs });
+      }
+
+      const result = await db.query(
+        "INSERT INTO schedules (username, day, start_at, end_at) VALUES($1, $2, $3, $4)",
+        [username, day, start_at, end_at]
+      );
+
+      res.redirect("/schedules");
+    } catch (error) {
+      console.log(error);
+    }
   }
-  
-});
+);
 
 module.exports = router;
