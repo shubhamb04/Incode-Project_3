92df4b676833afcf5ad0f758562e43435e8ac404
Shubham <69906688+shubhamb04@users.noreply.github.com>
2021-09-05 23:51:25 +1000

    Update database.js

--- db/database.js
+++ db/database.js
@@ -1,6 +1,6 @@
 const pgp = require('pg-promise')();
 
-const connection = 'postgres://postgres:Shubh2394$@localhost:5432/schedules'
+const connection = 'postgres://postgres:123456789@localhost:5432/schedules'
 
 const db = pgp(connection);
 
